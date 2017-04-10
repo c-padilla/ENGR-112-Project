55a38aeb088f33279ac024afa6646227f0c894fa
c-padilla <cpadilla@tamu.edu>
2017-04-10 02:07:36 -0500

    Update IDENTIFIER.m

--- IDENTIFIER.m
+++ IDENTIFIER.m
@@ -12,54 +12,56 @@ motorB.Speed = 15;
 color = input('Input color or materials: ', 's'); % lowercase only (red, blue, ..., steel_hdpe)
 howMany1 = input('Input quantity type1: '); % large (or steel)
 howMany2 = input('Input quantity type2: '); % small (or HDPE)... 
-% STEEL AND HDPE HAVE NOT BEEN IMPLEMENTED YET!
+% STEEL AND HDPE HAVE NOT BEEN IMPLEMENTED YET (mostly because idfk know how)
 
 total = howMany1 + howMany2;
 
-while (howMany1 ~= 0 && howMany2 ~= 0)    
-    if (mod(i, 2) == 1)    
-        if (howMany1 > 0)
-            start(motorA);
-            motorB.Speed = -15;
-            start(motorB);
-            
-            COLOR = readColorr(myev3, 1, ColorSensor1);
-            if (isequal(color, COLOR))
-                pause(2.2);
-                howMany1 = howMany1 - 1;
-                stop(motorA);
-                stop(motorB);
-                motorB.Speed = 15;
-            else
-                motorA.Speed = -20;
-                pause(2.5);
-                motorA.Speed = 20;
-            end
-            
-        end % big
-    
-    else % mod    
-        if (howMany2 > 0)
-            start(motorB);
+while (howMany1 ~= 0 && howMany2 ~= 0)        
+    if (howMany1 > 0)
+        start(motorA);
+        motorB.Speed = -15;
+        start(motorB);
+        
+        COLOR = readColorr(myev3, 1, ColorSensor1);
+        if (isequal(color, COLOR))
+            pause(2.2);
+            howMany1 = howMany1 - 1;
+            stop(motorA);
+            stop(motorB);
+            motorB.Speed = 15;
+        else
             motorA.Speed = -20;
-            start(motorA);
-            
-            COLOR = readColorr(myev3, 2, ColorSensor2);
-            if (isequal(color, COLOR))
-                pause(2.2);
-                howMany2 = howMany2 - 1;
-                stop(motorA);
-                stop(motorB);
-                motorA.Speed = 20;
-            else
-                motorB.Speed = -15;
-                pause(4);
-                motorB.Speed = 15;            
-            end
+            pause(2.5);
+            stop(motorA);
+            stop(motorB);
+            motorA.Speed = 20;
+            motorB.Speed = 15;
+        end
+    end % big
+    
+    if (howMany2 > 0)
+        start(motorB);
+        motorA.Speed = -20;
+        start(motorA);
         
-        end % small
+        COLOR = readColorr(myev3, 2, ColorSensor2);
+        if (isequal(color, COLOR))
+            pause(2.2);
+            howMany2 = howMany2 - 1;
+            stop(motorA);
+            stop(motorB);
+            motorA.Speed = 20;
+        else
+            motorB.Speed = -15;
+            pause(4);
+            stop(motorA);
+            stop(motorB);
+            motorB.Speed = 15;
+            motorA.Speed = 20;
+        end
         
-    end % mod if
-end % for loop
+    end % small
+    
+end % while loop
 
 fprintf('Done...\n\n\n\n also fuck you in the butt\n');
