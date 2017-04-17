29ed7730d85b99f364136b9bc91090df0a780a0f
c-padilla <cpadilla@tamu.edu>
2017-04-17 02:17:40 -0500

    Update IDENTIFIER.m

--- IDENTIFIER.m
+++ IDENTIFIER.m
@@ -6,27 +6,48 @@ motorB = motor(myev3, 'B');
 motorA.Speed = 20;
 ColorSensor1 = colorSensor(myev3, 1);
 ColorSensor2 = colorSensor(myev3, 2);
-motorB.Speed = 30;
+TouchSensor = touchSensor(myev3, 3);
+
+motorB.Speed = 20;
 color1=[];
 howMany1=[];
 howMany2=[];
 
-color1 = input('Input color or materials: ', 's'); % lowercase only (red, blue, ..., steel_hdpe)
-howMany1(1) = input('Input quantity type1: '); % large (or steel)
-howMany2(1) = input('Input quantity type2: '); % small (or HDPE)...
+color1, howMany1(1), howMany2(1) = readCard(1, myev3);
+while (readTouch(TouchSensor) == 0) 
+end
+color2, howMany1(2), howMany2(2) = readCard(2, myev3);
+%howMany1(2) = 0;
+%howMany2(2) = 0;
+%color2 = 'yellow';
+if(strcmp(color1, 'STEEL_HDPE')
+    if(howMany1(1) > 0 && howMany2(1) == 0)
+        color1 = 'steel';
+    elseif(howMany1(1) == 0 && howMany2(1) > 0)
+        color1 = 'HDPE';    
+    end
+end
 
-color2 = input('Input color or materials: ', 's'); % lowercase only (red, blue, ..., steel_hdpe)
-howMany1(2) = input('Input quantity type1: '); % large (or steel)
-howMany2(2) = input('Input quantity type2: '); % small (or HDPE)...
+if(strcmp(color2, 'STEEL_HDPE')
+    if(howMany1(2) > 0 && howMany2(2) == 0)
+        color2 = 'steel';
+    
+    elseif(howMany1(2) == 0 && howMany2(2) > 0)
+        color2 = 'HDPE';            
+    end
+end    
+% color1 = input('Input color or materials: ', 's'); % lowercase only (red, blue, ..., steel_hdpe)
+% howMany1(1) = input('Input quantity type1: '); % large (or steel)
+% howMany2(1) = input('Input quantity type2: '); % small (or HDPE)...
 
-% STEEL AND HDPE HAVE NOT BEEN IMPLEMENTED YET (mostly because idfk know how)
-
-total = howMany1 + howMany2;
+% color2 = input('Input color or materials: ', 's'); % lowercase only (red, blue, ..., steel_hdpe)
+% howMany1(2) = input('Input quantity type1: '); % large (or steel)
+% howMany2(2) = input('Input quantity type2: '); % small (or HDPE)...
 
 while (howMany1(1) ~= 0 || howMany2(1) ~= 0 || howMany1(2) ~= 0 || howMany2(2) ~= 0)        
     if (howMany1(1) > 0 || howMany1(2) > 0)
         start(motorA);
-        motorB.Speed = -30;
+        motorB.Speed = -20;
         start(motorB);
         
         COLOR = readColorr(myev3, 1, ColorSensor1);
@@ -35,20 +56,20 @@ while (howMany1(1) ~= 0 || howMany2(1) ~= 0 || howMany1(2) ~= 0 || howMany2(2) ~
             howMany1(1) = howMany1(1) - 1;
             stop(motorA);
             stop(motorB);
-            motorB.Speed = 30;
+            motorB.Speed = 20;
         elseif (isequal(color2, COLOR) && howMany1(2) > 0)
             pause(2.2);
             howMany1(2) = howMany1(2) - 1;
             stop(motorA);
             stop(motorB);
-            motorB.Speed = 30;
+            motorB.Speed = 20;
         else
             motorA.Speed = -20;
             pause(2.5);
             stop(motorA);
             stop(motorB);
             motorA.Speed = 20;
-            motorB.Speed = 30;
+            motorB.Speed = 20;
         end
     end % big
     
@@ -71,11 +92,11 @@ while (howMany1(1) ~= 0 || howMany2(1) ~= 0 || howMany1(2) ~= 0 || howMany2(2) ~
             stop(motorB);
             motorA.Speed = 20;    
         else
-            motorB.Speed = -25;
+            motorB.Speed = -20;
             pause(4);
             stop(motorA);
             stop(motorB);
-            motorB.Speed = 30;
+            motorB.Speed = 20;
             motorA.Speed = 20;
         end
         
